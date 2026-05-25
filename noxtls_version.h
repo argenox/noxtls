ba96541b23f22098e3ff42895b02b29870f33ed0
Jose Munoz <jose.munoz@argenox.com>
2026-05-25 16:29:22 +0000

    fix: updates to 0.2.52 and fixes configuration

--- noxtls_version.h
+++ noxtls_version.h
@@ -11,9 +11,9 @@
 /* Format: MAJOR.MINOR.PATCH */
 #define NOXTLS_VERSION_MAJOR 0
 #define NOXTLS_VERSION_MINOR 2
-#define NOXTLS_VERSION_PATCH 51
+#define NOXTLS_VERSION_PATCH 52
 
 /* Version string (e.g., "0.1.0") */
-#define NOXTLS_VERSION_STRING "0.2.51"
+#define NOXTLS_VERSION_STRING "0.2.52"
 
 #endif /* _NOXTLS_VERSION_H_ */
